c3febb5ce68b4cb65a37723549cdf27ca25ed82b
Krittanut Jirathawong <j.krittanut@gmail.com>
2020-02-12 22:15:37 +0700

    Tried out basic form/routing
    *body-parser might be required to run this version

--- NodejsBasic/routes/index.js
+++ NodejsBasic/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.post('/resolve', function (req, res) {
+  var credential = req.body.username + ' ' + req.body.password;
+  res.send('Login attempted with credential:' + credential);
+});
+
 module.exports = router;

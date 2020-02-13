4f25a7ccc6584069ffb0ff268fe5edef2917bbe9
Malzano <i.love.u.baby2353@gmail.com>
2020-02-13 16:47:46 +0700

    addDatabase

--- NodejsBasic/routes/index.js
+++ NodejsBasic/routes/index.js
@@ -1,7 +1,17 @@
 var express = require('express');
 var router = express.Router();
 const { check, validationResult } = require('express-validator');
+const monk = require('monk')
 
+// Connection URL
+const url = 'localhost:27017/TESTDB';
+
+const db = monk(url);
+
+
+db.then(() => {
+  console.log('Connected correctly to server klsdjfsldkfjldskfj')
+})
 // index page
 router.get('/', function(req, res, next) {
   res.render('index');
@@ -15,8 +25,18 @@ router.post('/resolve',[
   var errors = result.errors;
   if (!result.isEmpty()) {
     // username/password empty
+    
   } else {
+    var collection =db.get('test');
+    collection.insert([{a: 1}, {a: 2}, {a: 3}])
+    .then((docs) => {
+      // docs contains the documents inserted with added **_id** fields
+      // Inserted 3 documents into the document collection
+    }).catch((err) => {
+      // An error happened while inserting
+    }).then(() => db.close())
     // check username and password in db
+    
     var credential = req.body.username + ' ' + req.body.password;
     res.send('Login attempted with credential:' + credential);
   }

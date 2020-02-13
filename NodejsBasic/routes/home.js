ca4390dc58e45fb009740e102e70f7676f648104
ppalineee <s.palinee98@gmail.com>
2020-02-13 15:44:45 +0700

    add Tutor & Student contract pages

--- NodejsBasic/routes/home.js
+++ NodejsBasic/routes/home.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('home');
 });
 
+// tutor's contract page
+router.get('/tutors_contract', function(req, res, next) {
+  res.render('tutors_contract');
+});
+
+// student's contract page
+router.get('/students_contract', function(req, res, next) {
+  res.render('students_contract');
+});
+
 module.exports = router;
--- NodejsBasic/routes/index.js
+++ NodejsBasic/routes/index.js
@@ -1,14 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const { check, validationResult } = require('express-validator');
 
 // index page
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.post('/resolve', function (req, res) {
-  var credential = req.body.username + ' ' + req.body.password;
-  res.send('Login attempted with credential:' + credential);
+router.post('/resolve',[
+  check("username", "Enter username").not().isEmpty(),
+  check("password", "Enter password").not().isEmpty()
+], function (req, res) {
+  const result = validationResult(req);
+  var errors = result.errors;
+  if (!result.isEmpty()) {
+    // username/password empty
+  } else {
+    // check username and password in db
+    var credential = req.body.username + ' ' + req.body.password;
+    res.send('Login attempted with credential:' + credential);
+  }
 });
-
+  
 module.exports = router;

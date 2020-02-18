4723f59d7cc2b1c1fb1cc9797730706f9b814cc3
Krittanut Jirathawong <j.krittanut@gmail.com>
2020-02-18 13:08:13 +0700

    Auto-formatted codes (press option + shift + F on vscode on macos)

--- NodejsBasic/routes/index.js
+++ NodejsBasic/routes/index.js
@@ -7,75 +7,75 @@ const { check, validationResult } = require('express-validator');
 const uri = "mongodb+srv://malzano:019236055@seproject-zbimx.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
-  //const collection = client.db("SEproject").collection("usernamePassword");
-  // perform actions on the collection object
-  console.log("Successfully connected to online database");
-  client.close();
+	//const collection = client.db("SEproject").collection("usernamePassword");
+	// perform actions on the collection object
+	console.log("Successfully connected to online database");
+	client.close();
 });
 
- const dbName = 'SEproject'
+const dbName = 'SEproject'
 
 // index page
 router.get('/', function (req, res, next) {
-  res.render('index');
+	res.render('index');
 });
 
 router.post('/resolve', [
-  check("username", "Enter username").not().isEmpty(),
-  check("password", "Enter password").not().isEmpty()
+	check("username", "Enter username").not().isEmpty(),
+	check("password", "Enter password").not().isEmpty()
 ], function (req, res) {
-  const result = validationResult(req);
-  var errors = result.errors;
-  if (!result.isEmpty()) {
-    // username/password empty
-    res.send(errors[0]['msg']);
-    // send  error.msg if error 
-  } else {
-    client.connect(function (err) {
-      assert.equal(null, err);
-      const db = client.db(dbName);
-      db.collection('UserData').find({
-        'username' : req.body.username,
-        'password' : req.body.password
-      }).toArray(function(err,docs){
-        assert.equal(err,null);
-        console.log(docs);
-        // print log if found
-        if(docs.length ==0) res.send("Wrong username or Password");
-        else {
-          res.render('home');
-        }
-      })
-    });
-  }
+	const result = validationResult(req);
+	var errors = result.errors;
+	if (!result.isEmpty()) {
+		// username/password empty
+		res.send(errors[0]['msg']);
+		// send  error.msg if error 
+	} else {
+		client.connect(function (err) {
+			assert.equal(null, err);
+			const db = client.db(dbName);
+			db.collection('UserData').find({
+				'username': req.body.username,
+				'password': req.body.password
+			}).toArray(function (err, docs) {
+				assert.equal(err, null);
+				console.log(docs);
+				// print log if found
+				if (docs.length == 0) res.send("Wrong username or Password");
+				else {
+					res.render('home');
+				}
+			})
+		});
+	}
 });
 
 
 router.post('/register', [], function (req, res) {
 
-  //TODO: Check if the password and confirmed password match
-  //TODO: Check inputs in general
+	//TODO: Check if the password and confirmed password match
+	//TODO: Check inputs in general
 
-  client.connect(function (err) {
-    //checks for connection error
-    assert.equal(null, err);
+	client.connect(function (err) {
+		//checks for connection error
+		assert.equal(null, err);
 
-    //once connected, add a doc to collection 'UserData'
-    const db = client.db(dbName);
+		//once connected, add a doc to collection 'UserData'
+		const db = client.db(dbName);
 
-    db.collection('UserData').insertOne({
-      position:req.body.position,
-      firstname:req.body.firstname,
-      lastname:req.body.lastname,
-      username:req.body.username,
-      password:req.body.password,
-      phone:req.body.phone,
-      email:req.body.email,
-      gender:req.body.gender
-    });
-  });
+		db.collection('UserData').insertOne({
+			position: req.body.position,
+			firstname: req.body.firstname,
+			lastname: req.body.lastname,
+			username: req.body.username,
+			password: req.body.password,
+			phone: req.body.phone,
+			email: req.body.email,
+			gender: req.body.gender
+		});
+	});
 
-  res.render('home');
+	res.render('home');
 });
 
 module.exports = router;

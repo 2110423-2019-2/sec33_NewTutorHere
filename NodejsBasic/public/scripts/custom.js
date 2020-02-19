58ae7db6958da810424d9101446e1c5c4fb9a61b
SPRYK <Seth214@hotmail.co>
2020-02-19 19:14:00 +0700

    add js file but cannot use

---
+++ NodejsBasic/public/scripts/custom.js
@@ -0,0 +1,31 @@
+function generateContract() {
+    
+    var n = 2, // number 0f contract
+        i = 1;
+
+    for (i = 1; i <= n; i++) {
+        var data = {
+
+            name: 'Seth Poophak'
+
+        }
+        var template = [
+            '<form class="form" id="contract">',
+            '<img class="nav-img-user" src="/img/user.png" style="height: 5em;" />',
+            '<div class="c-box be-right">',
+            '<div class="blockcontract">',
+            '<span class="name-stucontract">',
+            '<a>sends a contract to</a>',
+            '<p>{{name}}</p>', // student name
+            '</span>',
+            '<br />',
+            '<button class="view-button search-button be-right" >Cancel this contract</button>',
+            '<button class="view-button search-button be-right" >Accept this contract</button>',
+            '</div>',
+            '</div>',
+            '</form>'
+        ].join("\n");
+        var html = Mustache.render(template, data);
+        $("#contract-form").append(html);
+    }
+}
